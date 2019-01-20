25de63e1936ba911fcb1925de5c8d96249ff0222
Jiangfuquan <jfq3301@163.com>
2019-01-20 20:03:06 +0800

    "no git add step."

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,6 @@ class Helloworld
 	public static void main(String[] args)
 	{
 		System.out.println("Helloworld!");
-		System.out.println("Helloworld!");
+		// System.out.println("Helloworld!");
 	}
 }
