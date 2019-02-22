9b026d204fc2997fc6d4d8ef966e9e7b84edcf9b
jfq3301 <jfq3301@163.com>
2019-02-22 20:43:50 +0800

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,7 @@ class Helloworld
 	public static void main(String[] args)
 	{
 		System.out.println("Helloworld!");
+		System.out.println("Hello world. jfq. anthor start. gogogo.");
 		// System.out.println("Helloworld!");
 	}
 }
