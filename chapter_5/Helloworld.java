272dddabccbc4b28d956b3be31ea13b6eaa79bc4
jfq <jfq3301@163.com>
2019-04-04 20:22:01 +0800

    know package usage.

---
+++ chapter_5/Helloworld.java
@@ -0,0 +1,11 @@
+package jfq.java;
+
+class Helloworld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Helloworld!");
+		System.out.println("Hello world. jfq. anthor start. gogogo.");
+		// System.out.println("Helloworld!");
+	}
+}
